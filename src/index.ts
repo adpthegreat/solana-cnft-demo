e8d11a532b0b24fa3285772d9dbd8ffbbccc719d
adpthegreat <adpthegreatanddefiant@gmail.com>
2024-10-01 23:31:54 +0100

    chore: - added try catch for getOrCreateCollectionNFT - shortened uri.ts, - encapsulated index.ts logic in one function

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,34 @@
-
- import { Connection,
-  Keypair,
-  PublicKey,
-  Transaction,
-  clusterApiUrl,
-  sendAndConfirmTransaction,
-} from "@solana/web3.js"
-import {
-  getOrCreateKeypair,
-  createNftMetadata,
-  CollectionDetails,
-  getOrCreateCollectionNFT,
-  airdropSolIfNeeded,
-} from "./utils"
-import {
-  ValidDepthSizePair,
-} from "@solana/spl-account-compression"
-import {
-  LeafSchema,
-  mintV1,
-  transfer,
-  getAssetWithProof,
-  createTree,
-  mintToCollectionV1,
-  parseLeafFromMintV1Transaction,
-  fetchMerkleTree,
-  TokenStandard,
-  mplBubblegum,
-  findLeafAssetIdPda,
-} from "@metaplex-foundation/mpl-bubblegum";
+import dotenv from "dotenv";
+import { clusterApiUrl } from "@solana/web3.js";
+import { getOrCreateKeypair, airdropSolIfNeeded } from "./utils";
+import { mplBubblegum } from "@metaplex-foundation/mpl-bubblegum";
 import { dasApi } from "@metaplex-foundation/digital-asset-standard-api";
-// import { BN } from "@coral-xyz/anchor"
-import dotenv from "dotenv"
-import { BN } from "@coral-xyz/anchor"
-import { getExplorerLink } from "@solana-developers/helpers"
-import { generateSigner, keypairIdentity, publicKey } from "@metaplex-foundation/umi"
-import { PublicKey as UmiPublicKey } from "@metaplex-foundation/umi-public-keys";
+import * as anchor from "@coral-xyz/anchor";
+import { keypairIdentity, Keypair } from "@metaplex-foundation/umi";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { base58 } from "@metaplex-foundation/umi/serializers";
+import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 
+export const initializeUmi = async () => {
+  // const {BN} = anchor.default;
 
-dotenv.config()
+  const umi = createUmi(clusterApiUrl("devnet"));
 
-const umi = createUmi(clusterApiUrl("devnet"));
+  //get keypair from .env file or create a new one
+  const wallet = await getOrCreateKeypair("Wallet1");
 
-//get keypair from .env file or create a new one
-const wallet = await getOrCreateKeypair("Wallet1");
+  await airdropSolIfNeeded(wallet.publicKey);
 
-// convert to Umi compatible keypair
-const umiKeypair = umi.eddsa.createKeypairFromSecretKey(wallet.secretKey);
+  // convert to Umi compatible keypair
+  const umiKeypair = umi.eddsa.createKeypairFromSecretKey(wallet.secretKey);
 
-// Load the DAS API and MPL Bubblegum plugins into Umi, and set the Umi identity using a keypair,
-// which acts as the signer for transactions.
-umi.use(keypairIdentity(umiKeypair)).use(mplBubblegum()).use(dasApi());
+  // Load the DAS API and MPL Bubblegum plugins into Umi, and set the Umi identity using a keypair,
+  // which acts as the signer for transactions.
+  umi
+    .use(keypairIdentity(umiKeypair))
+    .use(mplTokenMetadata())
+    .use(mplBubblegum())
+    .use(dasApi());
 
-airdropSolIfNeeded(wallet.publicKey);
-
-
-
-
+  return umi;
+};
--- src/utils.ts
+++ src/utils.ts
@@ -14,25 +14,24 @@ import {
 } from "@metaplex-foundation/mpl-bubblegum";
 import { PublicKey as UmiPublicKey } from "@metaplex-foundation/umi-public-keys";
 import { some } from "@metaplex-foundation/umi-options";
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import {
   createNft,
   fetchMetadataFromSeeds,
   findMasterEditionPda,
   findMetadataPda,
-  mplTokenMetadata,
 } from "@metaplex-foundation/mpl-token-metadata";
 import { uris } from "./uri";
 import {
   getKeypairFromEnvironment,
   addKeypairToEnvFile,
   airdropIfRequired,
+  getExplorerLink,
 } from "@solana-developers/helpers";
 import {
   generateSigner,
-  keypairIdentity,
   percentAmount,
   publicKey,
+  Umi,
 } from "@metaplex-foundation/umi";
 
 export async function getOrCreateKeypair(walletName: string): Promise<Keypair> {
@@ -45,7 +44,7 @@ export async function getOrCreateKeypair(walletName: string): Promise<Keypair> {
     console.log("Retrieved wallet from .env file successfully! ✅");
   } catch (error) {
     // If keypair doesn't exist in .env, generate a new keypair and store it
-    console.log(`Writing ${walletName} Keypair to .env file...`);
+    console.log(`Writing ${walletName} keypair to .env file...`);
 
     // Generate a new keypair
     keypair = Keypair.generate();
@@ -54,8 +53,9 @@ export async function getOrCreateKeypair(walletName: string): Promise<Keypair> {
     await addKeypairToEnvFile(keypair, walletName);
 
     console.log(`${walletName} PublicKey: ${keypair.publicKey.toBase58()}`);
-    console.log("Created wallet successfully! ✅")
+    console.log("Created wallet successfully! ✅");
   }
+
   return keypair;
 }
 
@@ -66,17 +66,21 @@ export async function airdropSolIfNeeded(publicKey: PublicKey) {
     const newBalance = await airdropIfRequired(
       connection,
       publicKey,
-      2 * LAMPORTS_PER_SOL, // Airdrop 2 SOL if needed
-      1 * LAMPORTS_PER_SOL // Only airdrop if the balance is below 1 SOL
+      5 * LAMPORTS_PER_SOL, // Airdrop 5 SOL if needed
+      1.5 * LAMPORTS_PER_SOL // Only airdrop if the balance is below 1.5 SOL
     );
 
-    console.log("New balance is", newBalance / LAMPORTS_PER_SOL, "SOL");
+    console.log("New balance is", newBalance / LAMPORTS_PER_SOL);
   } catch (error) {
     console.log("Airdrop Unsuccessful, likely rate-limited. Try again later.");
   }
 }
 
-export function createNftMetadata(creator: UmiPublicKey, index: number) {
+export function createNftMetadata(
+  creator: UmiPublicKey,
+  index: number,
+  collectionMint: UmiPublicKey
+) {
   if (index > uris.length) {
     throw new Error("Index is out of range");
   }
@@ -85,8 +89,8 @@ export function createNftMetadata(creator: UmiPublicKey, index: number) {
 
   // Compressed NFT Metadata
   const compressedNFTMetadata: MetadataArgs = {
-    name: "CNFT",
-    symbol: "CNFT",
+    name: "Collection NFT",
+    symbol: "cNFT",
     uri: uri,
     creators: [{ address: creator, verified: false, share: 100 }],
     editionNonce: some(0),
@@ -96,12 +100,12 @@ export function createNftMetadata(creator: UmiPublicKey, index: number) {
       total: BigInt(5), // Total of 5 uses originally
     }),
     collection: some({
-      verified: true,
-      key: creator,
+      key: collectionMint,
+      verified: false,
     }),
     primarySaleHappened: false,
-    sellerFeeBasisPoints: 0,
-    isMutable: false,
+    sellerFeeBasisPoints: 10,
+    isMutable: true,
     tokenProgramVersion: TokenProgramVersion.Original,
     tokenStandard: some(TokenStandard.NonFungible),
   };
@@ -115,70 +119,57 @@ export type CollectionDetails = {
   masterEditionAccount: UmiPublicKey;
 };
 
-//Loads a collection NFT from the .env file. If the collection NFT doesn't exist,
-// it creates a new one using a random URI from a predefined list, then appends
-// the mint address to .env
-export async function getOrCreateCollectionNFT(
-  payer: Keypair
-): Promise<CollectionDetails> {
-  // Retrieve the collection NFT mint address from the .env file if it exists
+export async function getOrCreateCollectionNFT(umi: Umi) {
+  try {
   const envCollectionNftAddress = process.env["COLLECTION_NFT"];
-
-  // Initialize the Umi instance with the Solana devnet cluster as the target network
-  const umi = createUmi(clusterApiUrl("devnet"));
-
-  // convert to Umi compatible keypair
-  const umiKeypair = umi.eddsa.createKeypairFromSecretKey(payer.secretKey);
-
-  // load the MPL metadata program plugin and assign a signer to our umi instance
-  umi.use(keypairIdentity(umiKeypair)).use(mplTokenMetadata());
-
   const collectionMint = generateSigner(umi);
 
-  // Function to fetch addresses for an existing collection NFT
+  // Helper function to log explorer links
+  const logExplorerLinks = (
+    mint: UmiPublicKey,
+    metadata: UmiPublicKey,
+    masterEdition: UmiPublicKey
+  ) => {
+    console.log(
+      "Mint Address:",
+      getExplorerLink("address", mint, "devnet"),
+      "\n"
+    );
+    console.log(
+      "Metadata Address:",
+      getExplorerLink("address", metadata, "devnet"),
+      "\n"
+    );
+    console.log(
+      "Master Edition Account:",
+      getExplorerLink("address", masterEdition, "devnet"),
+      "\n"
+    );
+  };
+
+  // Helper function to fetch metadata and master edition addresses for a given mint
   const fetchCollectionAddresses = async (
     collectionNftAddress: UmiPublicKey
   ) => {
-    //fetch meta data address
-    // Derive the Metadata PDA (Program Derived Address) for the given mint address (collectionNftAddress).
-    // The metadata account stores the onchain data for the NFT, such as name, symbol, URI, etc.
-    // This address is deterministically generated using the mint address and the Token Metadata program ID.
-    // The resulting PDA is needed to fetch or interact with the NFT's metadata.
     const metadataAddress = findMetadataPda(umi, {
       mint: collectionNftAddress,
     })[0];
-
-    //fetch masterEdition Address
-    // Derive the MasterEdition PDA (Program Derived Address) for the given mint address (collectionNftAddress).
-    // The MasterEdition account manages the NFT's supply and editioning information (if the NFT supports editions).
-    // This PDA is deterministically generated using the mint address and the Token Metadata program ID.
-    // The resulting address is necessary to fetch or interact with the NFT's MasterEdition data.
-
     const masterEditionAddress = findMasterEditionPda(umi, {
       mint: collectionNftAddress,
     })[0];
-
     return { metadataAddress, masterEditionAddress };
   };
 
+  // If collection NFT address exists in .env, fetch its details and return
   if (envCollectionNftAddress) {
-    // The address of the mint account
     const collectionNftAddress = publicKey(envCollectionNftAddress);
-
-    // fetchMetadataFromSeeds get mint address
     const collectionNft = await fetchMetadataFromSeeds(umi, {
       mint: collectionNftAddress,
     });
-
     const { metadataAddress, masterEditionAddress } =
       await fetchCollectionAddresses(collectionNftAddress);
 
-    // if (collectionNft.name !== "nft") {
-    //   throw new Error("Invalid collection NFT");
-    // }
-    // if (collectionNft.model !== "nft") {
-    //   throw new Error("Invalid collection NFT");
-    // }
+    logExplorerLinks(collectionNft.mint, metadataAddress, masterEditionAddress);
 
     return {
       mint: collectionNft.mint,
@@ -187,7 +178,7 @@ export async function getOrCreateCollectionNFT(
     };
   }
 
-  // Select a random URI from uris
+  // If no collection NFT exists, create a new one
   const randomUri = uris[Math.floor(Math.random() * uris.length)];
 
   await createNft(umi, {
@@ -196,30 +187,31 @@ export async function getOrCreateCollectionNFT(
     uri: randomUri,
     authority: umi.identity,
     updateAuthority: umi.identity.publicKey,
-    sellerFeeBasisPoints: percentAmount(0),
-    symbol: "Collection",
+    sellerFeeBasisPoints: percentAmount(10),
+    symbol: "cNFT",
     isMutable: true,
     isCollection: true,
   }).sendAndConfirm(umi, { send: { commitment: "finalized" } });
 
   const collectionNftAddress = collectionMint.publicKey;
-
-  // fetchMetadataFromSeeds get mint address
   const collectionNft = await fetchMetadataFromSeeds(umi, {
     mint: collectionNftAddress,
   });
-
   const { metadataAddress, masterEditionAddress } =
     await fetchCollectionAddresses(collectionNftAddress);
 
-  // public keys in umi are now directly represented in base58
-  fs.appendFileSync(".env", `\n${"COLLECTION_NFT"}=${collectionNft.mint})}`);
+  // Save the new collection NFT to .env
+  fs.appendFileSync(".env", `\nCOLLECTION_NFT=${collectionNft.mint}`);
 
+  logExplorerLinks(collectionNft.mint, metadataAddress, masterEditionAddress);
+  // wrap in try catch , add success message
   return {
     mint: collectionNft.mint,
     metadata: metadataAddress,
     masterEditionAccount: masterEditionAddress,
   };
+} catch(error:any) {
+    console.error("Failed to create or fetch Collection NFT ❌", error.message);
+    throw new Error("Error occurred during NFT creation or fetching process.");
+  }
 }
-
-
